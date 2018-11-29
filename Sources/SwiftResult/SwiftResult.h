c4395d6d2c406377353708761cc57ac05fb4be6d
Yuta Koshizawa <koher@koherent.org>
2018-11-29 14:46:27 +0900

    Support iOS, macOS, tvOS and watchOS

---
+++ Sources/SwiftResult/SwiftResult.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftResult.
+FOUNDATION_EXPORT double SwiftResultVersionNumber;
+
+//! Project version string for SwiftResult.
+FOUNDATION_EXPORT const unsigned char SwiftResultVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftResult/PublicHeader.h>
+
+
